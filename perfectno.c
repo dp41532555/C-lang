3e94882d306a8b233b97adeccd8f4205c528f7f1
divya <dp41532555@gmail.com>
2022-11-10 09:28:36 +0530

    10-11-22

--- perfectno.c
+++ perfectno.c
@@ -1,32 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int prf(int num)
 {
-    
-    int value,i,a=0;
-  
-    for(i=1;i<num;i++)
-    {
-        if(num%i==0)
-        {
 
-        a=a+i;
+    int value, i, a = 0;
+
+    for (i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            a = a + i;
         }
     }
-    if(num == a)
+    if (num == a)
     {
-    printf("num is perfect");
+        printf("num is perfect");
     }
     else
     {
         printf("num is not perfect");
     }
-    
 }
 int main()
 {
     int value;
-     printf("\n enter number : ");
-    scanf("%d",&value);
+    printf("\n enter number : ");
+    scanf("%d", &value);
     prf(value);
-
 }
