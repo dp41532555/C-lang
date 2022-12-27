e8778739985f7eb8bc8e528c04896a993bf25387
divya <dp41532555@gmail.com>
2022-12-27 08:57:40 +0530

    27-12-2022

---
+++ multiple_inheritance.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class base
+{
+
+public:
+    int n;
+    base()
+    {
+        cout << "enter num :  " << endl;
+        cin >> n;
+    }
+};
+class base1
+{
+public:
+    int i;
+};
+
+class derived : public base, public base1
+{
+
+public:
+    cube()
+    {
+        for (i = 0; i < n; i++)
+        {
+            cout << "cube " << i << ":-" << i * i * i << endl;
+        }
+    }
+    square()
+    {
+        for (i = 0; i < n; i++)
+        {
+            cout << "square" << i << ":-" << i * i << endl;
+        }
+    }
+};
+
+int main()
+{
+    derived a;
+    a.cube();
+    cout << "=========================================================" << endl;
+    a.square();
+}
