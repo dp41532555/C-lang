49a0617b86f500ec1fd4739fc9e4c1c78c188a08
divya <dp41532555@gmail.com>
2023-01-10 09:48:56 +0530

    10-01-2023

---
+++ data structure/1_basic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+
+    //int i=10;
+    //printf("i:%d\ti:%d\ti:%d",++i,++i,i++);
+    //printf("i:%d\ti:%d\ti:%d",i++,++i,i++);
+    //printf("i:%d\ti:%d\ti:%d",i,++i,i++);
+    //printf("i:%d\ti:%d",i,++i);
+    //printf("i:%d",++i);
+}
