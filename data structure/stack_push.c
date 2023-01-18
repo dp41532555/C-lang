cba59fe8701bd7513b9c75d208771de90c32bbc5
divya <dp41532555@gmail.com>
2023-01-18 09:56:29 +0530

    18-01-2023

--- data structure/stack_push.c
+++ data structure/stack_push.c
@@ -1,34 +1,86 @@
-//stack operation
-#include<stdio.h>
+// stack operation
+#include <stdio.h>
 #define n 5
 int i;
-int top=-1;
+int top = -1;
 int display(int a[])
 {
-    for(i=0;i<top;i++)
+    for (i = 0; i <= top; i++)
     {
-        printf("%d",a[i]);
+        printf("%d ", a[i]);
     }
     printf("\n");
 }
-int insert(int a[],int val)
+int insert(int a[], int val)
 {
-    if(top>=n-1)
+    if (top >= n - 1)
         printf("array is overflow.....\n");
 
     else
     {
         ++top;
-        a[top]=val;
+        a[top] = val;
     }
+}
+int Firstinsert(int a[], int val)
+{
+    if (top >= n - 1)
+        printf("array is overflow.....\n");
+
+    else
+    {
+        for(i=n-1;i>=0;i--)
+        {
+            a[i] = a[i-1];
+        }
+        a[0] = val;
+        ++top;
+    }
+}
+int Firstdelete(int a[], int val)
+{
+    if (top <0)
+        printf("array is overflow.....\n");
+
+    else
+    {
+       for(i=0;i<n;i++)
+        {
+            a[i-1] = a[i];
+            
+        }
+         a[i] = val;
+         top--;
+           
+    }
+}
+
+int pop(int a[])
+{
+    if (top < 0 )
+    {
+        printf("Stack Underflow");   
+    }
+    else
+    {
+        top--;
+    }
+    
 }
 int main()
 {
     int a[n];
-    insert(a,10);
-    insert(a,20);
-    insert(a,30);
-    insert(a,40);
-    insert(a,50);
+    insert(a, 10);
+    insert(a, 20);
+    insert(a, 30);
+    insert(a, 40);
+    display(a);
+    // pop(a);
+    // pop(a);
+    //Firstinsert(a,50);
+   // Firstinsert(a,60);
+    Firstdelete(a,10);
+ Firstdelete(a,20); 
+
     display(a);
 }
